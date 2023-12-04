7e8df7d8204eed6062fad7ae1895ddeb2e70bbea
egiK1337 <117719087+egiK1337@users.noreply.github.com>
2023-12-04 11:23:57 +0300

    add FormatData and InputProcess
    validation numbers

---
+++ ThirdApp/ThirdApp/InputException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdApp
+{
+    public class InputException : Exception
+    {
+        public Severity Severity { get; set; }
+        public Dictionary<string, string> Data { get; set; }
+
+        public InputException(string message, Severity severity, Dictionary<string, string> data) 
+            : base(message)
+        {
+            Severity = severity;
+            Data = data;
+        }
+    }
+}
--- ThirdApp/ThirdApp/Program.cs
+++ ThirdApp/ThirdApp/Program.cs
@@ -1,10 +1,98 @@
-﻿namespace ThirdApp
+﻿using System.Collections;
+
+namespace ThirdApp
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static IDictionary<string, string> FormatData(string message, Severity severity, IDictionary<string, string> data)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(message);
+
+            foreach (var item in data)
+            {
+                Console.WriteLine($"{item.Key} = {item.Value}");
+            }
+
+            return data;
+        }
+
+        public static int InputProcess()
+        {
+            var data = new Dictionary<string, string>();
+
+            var count = 0;
+
+            Console.WriteLine("Введите значение переменной a, только положительное число");
+            data["a"] = Console.ReadLine();
+
+            Console.WriteLine("Введите значение переменной b");
+            data["b"] = Console.ReadLine();
+
+            Console.WriteLine("Введите значение переменной c");
+            data["c"] = Console.ReadLine();
+
+            if (!int.TryParse(data["a"], out _) || int.Parse(data["a"]) == 0)
+            {
+                count++;
+                throw new InputException("Неверное значение переменной а", Severity.Error, data);
+            }
+            if (!int.TryParse(data["b"], out _))
+            {
+                count++;
+                throw new InputException("Неверное значение переменной b", Severity.Error, data);
+
+            }
+            if (!int.TryParse(data["c"], out _))
+            {
+                count++;
+                throw new InputException("Неверное значение переменной c", Severity.Error, data);
+            }
+            return count;
+        }
+
+        static void SolvingQuadraticEquation(IDictionary<string, string> data)
+        {
+            int.TryParse(data["a"], out var a);
+            int.TryParse(data["b"], out var b);
+            int.TryParse(data["c"], out var c);
+
+            var d = b * b - 4 * a * c;
+
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Необходимо решить квадратное уравнение вида: a * x ^ 2 + b * x + c = 0");
+
+            while (true)
+            {
+                try
+                {
+                    if (InputProcess() == 0)
+                    {
+                        break;
+                    }
+                    InputProcess();
+                }
+                catch (InputException e)
+                {
+                  var data = FormatData(e.Message, e.Severity, e.Data);
+
+                    try
+                    {
+                        SolvingQuadraticEquation(data);
+                    }
+                    catch
+                    {
+
+                    }
+                }
+
+              
+            }
+
+            
+
         }
     }
 }

14167ca853915450d85c631f76ea5877b6ef4038
egiK1337 <117719087+egiK1337@users.noreply.github.com>
2023-12-05 09:12:23 +0300

    finish
    solved an issue

--- ThirdApp/ThirdApp/Program.cs
+++ ThirdApp/ThirdApp/Program.cs
@@ -1,98 +1,134 @@
-﻿using System.Collections;
-
+﻿
 namespace ThirdApp
 {
     internal class Program
     {
-        public static IDictionary<string, string> FormatData(string message, Severity severity, IDictionary<string, string> data)
+        public static void FormatData(string message, Severity severity, IDictionary<string, string> data)
         {
+            if(severity == Severity.Error)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            if(severity == Severity.Warning) 
+            {
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("-----");
+            }
+
+            Console.WriteLine();
             Console.WriteLine(message);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("-----");
+            }
+
+            Console.WriteLine();
 
             foreach (var item in data)
             {
                 Console.WriteLine($"{item.Key} = {item.Value}");
             }
 
-            return data;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
         }
 
-        public static int InputProcess()
+        public static Dictionary<string, string> InputProcess()
         {
             var data = new Dictionary<string, string>();
 
-            var count = 0;
-
-            Console.WriteLine("Введите значение переменной a, только положительное число");
+            Console.WriteLine("Введите числовое значение переменной a, не равное 0");
             data["a"] = Console.ReadLine();
 
-            Console.WriteLine("Введите значение переменной b");
+            Console.WriteLine("Введите числовое значение переменной b");
             data["b"] = Console.ReadLine();
 
-            Console.WriteLine("Введите значение переменной c");
+            Console.WriteLine("Введите числовое значение переменной c");
             data["c"] = Console.ReadLine();
 
             if (!int.TryParse(data["a"], out _) || int.Parse(data["a"]) == 0)
             {
-                count++;
                 throw new InputException("Неверное значение переменной а", Severity.Error, data);
             }
             if (!int.TryParse(data["b"], out _))
             {
-                count++;
                 throw new InputException("Неверное значение переменной b", Severity.Error, data);
-
             }
             if (!int.TryParse(data["c"], out _))
             {
-                count++;
                 throw new InputException("Неверное значение переменной c", Severity.Error, data);
             }
-            return count;
+            return data;
         }
 
-        static void SolvingQuadraticEquation(IDictionary<string, string> data)
+        static void SolvingQuadraticEquation(Dictionary<string, string> data)
         {
             int.TryParse(data["a"], out var a);
             int.TryParse(data["b"], out var b);
             int.TryParse(data["c"], out var c);
 
-            var d = b * b - 4 * a * c;
+            double d = (b * b) - (4 * a * c);
 
+            var x1 = 0.0;
+            var x2 = 0.0;
+
+            if (d < 0)
+            {
+                throw new QuadraticEquationException("Вещественных значений не найдено", Severity.Warning, data);
+            }
+            if (d == 0)
+            {
+                x1 = -b / (2 * (double)a);
+                Console.WriteLine($"Квадратное уравнение имеет один корень {x1}");
+            }
+            if (d > 0)
+            {
+                x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                Console.WriteLine($"Квадратное уравнение имеет два корня x1 = {x1}, x1 = {x2}");
+            }
         }
 
         static void Main()
         {
             Console.WriteLine("Необходимо решить квадратное уравнение вида: a * x ^ 2 + b * x + c = 0");
 
+            Dictionary<string, string> dt = new Dictionary<string, string>();
+
             while (true)
             {
                 try
                 {
-                    if (InputProcess() == 0)
+                    var inputProcess = InputProcess();
+
+                    if (inputProcess.Count == 3)
                     {
-                        break;
+                        foreach (var item in inputProcess)
+                        {
+                            dt.Add(item.Key, item.Value);
+                        }
+                        SolvingQuadraticEquation(dt);
                     }
-                    InputProcess();
                 }
                 catch (InputException e)
                 {
-                  var data = FormatData(e.Message, e.Severity, e.Data);
-
-                    try
-                    {
-                        SolvingQuadraticEquation(data);
-                    }
-                    catch
-                    {
-
-                    }
+                    FormatData(e.Message, e.Severity, e.Data);
                 }
-
-              
+                catch (QuadraticEquationException f)
+                {
+                    FormatData(f.Message, f.Severity, f.Data);
+                }
+                dt.Clear();
             }
-
-            
-
         }
     }
 }
+
---
+++ ThirdApp/ThirdApp/QuadraticEquationException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdApp
+{
+    public class QuadraticEquationException : Exception
+    {
+        public Severity Severity { get; set; }
+        public Dictionary<string, string> Data { get; set; }
+
+        public QuadraticEquationException(string message, Severity severity, Dictionary<string, string> data)
+            : base(message)
+        {
+            Severity = severity;
+            Data = data;
+        }
+    }
+}
